d9f954b49967a5aa450502015c224e62407f475f
sultana-nadia <nadiamessi666@gmail.com>
2024-02-19 18:18:15 +0600

    cupon

--- script/tickets.js
+++ script/tickets.js
@@ -5,8 +5,8 @@ for (const btn of allBtn){
     btn.addEventListener("click", function(event){
         count = count + 1;
 
-        const ticketName = event.target.childNodes[0];
-        const ticketPrice = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[5].childNodes[3].childNodes[1].childNodes[1].childNodes[9].childNodes[3].childNodes[0];
+        const ticketName = event.target.childNodes[0].textContent;
+        const ticketPrice = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[5].childNodes[3].childNodes[1].childNodes[1].childNodes[9].childNodes[3].childNodes[0].textContent;
 
         const selectedContainer = document.getElementById("selected-place-container");
 
@@ -17,29 +17,44 @@ for (const btn of allBtn){
         const p2 = document.createElement("p");
         p2.innerText = ticketPrice;
 
+        event.target.style.backgroundColor = "lightgreen";
+
         li.appendChild(p);
         li.appendChild(p2);
         selectedContainer.appendChild(li);
 
 
+        totalPrice("total-price", parseInt(ticketPrice));
+
+        grandTotalPrice("grand-total-price", parseInt(ticketPrice));
 
         setInnerText("cart-count", count);
     });
 }
+
+function totalPrice(id, value){
+    const totalPrice = document.getElementById(id).innerText;
+    const convertedTotalCost = parseInt(totalPrice);
+    const sum = convertedTotalCost + parseInt(value);
+    setInnerText(id, sum);
+}
+
+// grand total price
+function grandTotalPrice(){
+    const updatePrice = document.getElementById("total-price").innerText;
+    const convertedTotalCost = parseInt(updatePrice);
+
+    setInnerText("grand-total-price", convertedTotalCost);
+}
+
+
 function setInnerText(id, value){
     document.getElementById(id).innerText = value;
 }
 
-// availableseat ar price
-const addButtons = document.querySelectorAll(".add-btn");
 
-let totalPrice = 0;
-let grandTotalPrice = 0;
-
-function updatePrices(){
-    document.getElementById('total-price').innerText = 'BDT' + totalPrice;
-    document.getElementById('grand-total-price').innerText = 'BDT' + grandTotalPrice;
-}
+// available seat
+const addButtons = document.querySelectorAll(".add-btn")
 
 for(let i = 0; i < addButtons.length; i++){
     addButtons[i].addEventListener('click', function() {
@@ -48,33 +63,10 @@ for(let i = 0; i < addButtons.length; i++){
         if(currentSeats > 0){
             currentSeats--;
             document.getElementById('current-seat').innerText = currentSeats;
-
-            totalPrice += 550;
-            grandTotalPrice += 550;
-
-            updatePrices();
         } 
     });
 }
-// appendchild
 
 
-
-// decrease seats
-// const addButtons = document.querySelectorAll(".add-btn");
-
-// for (let i = 0; i < addButtons.length; i++) {
-//     addButtons[i].addEventListener('click', function() {
-//         let currentSeats = parseInt(document.getElementById('current-seat').innerText);
-
-//         if (currentSeats > 0) {
-//             currentSeats--;
-//             document.getElementById('current-seat').innerText = currentSeats;
-//         } 
-//         else{
-//             alert('No more available seats!');
-//         }
-//     });
-// }
 
 
